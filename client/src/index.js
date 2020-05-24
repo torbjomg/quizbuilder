3afe1a98b1ff2234516e9f7889f7d2adafd64b9f
torbjomg <torbjorngul@gmail.com>
2020-05-24 19:51:13 +0200

    test

--- client/src/index.js
+++ client/src/index.js
@@ -18,7 +18,7 @@ const onRedirectCallback = (appState) => {
 
 ReactDOM.render(
   <Auth0Provider
-    domain={process.env.AUTH0_DOMAIN}
+    domain={process.env.AUTH0_DOMAIN || "dummy"}
     client_id={process.env.AUTH0_CLIENT_ID}
     redirect_uri={window.location.origin}
     onRedirectCallback={onRedirectCallback}
