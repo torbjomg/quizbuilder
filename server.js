83a354b8dcd1abba6e7970fb3df438e1a3e2468a
torbjomg <torbjorngul@gmail.com>
2020-05-24 14:14:01 +0200

    use mongodb atlas, dotenv

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const router = require("./routes/index");
+require("dotenv").config();
 
 const app = express();
-const PORT = 3001;
-const MONGODB_URI = "mongodb://localhost:27017/quibuilder_db";
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI =
+  process.env.MONGODB_CONNECTION_STRING ||
+  "mongodb://localhost:27017/quibuilder_db";
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/api", router);
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  MONGODB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  },
+  (err) => {
+    if (err) throw err;
+  }
+);
 mongoose.connection.once("open", function () {
   console.log("Connected to db");
 });
