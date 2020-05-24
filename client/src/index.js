f79b4f0e366311e57a1cad472a77cd4f98966c17
torbjomg <torbjorngul@gmail.com>
2020-05-24 16:41:58 +0200

    move auth config to dotenv

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,9 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Auth0Provider } from "./react-auth0-spa";
-import config from "./auth_config.json";
 import history from "./utils/history";
 import "bootstrap/dist/css/bootstrap.css";
+require("dotenv").config();
 
 const onRedirectCallback = (appState) => {
   history.push(
@@ -18,8 +18,8 @@ const onRedirectCallback = (appState) => {
 
 ReactDOM.render(
   <Auth0Provider
-    domain={config.domain}
-    client_id={config.clientId}
+    domain={process.env.AUTH_DOMAIN}
+    client_id={process.env.AUTH_CLIENT_ID}
     redirect_uri={window.location.origin}
     onRedirectCallback={onRedirectCallback}
   >
