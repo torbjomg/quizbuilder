d8efd19f1c6eb3fc0be2dccd2b014b3138d11771
torbjomg <torbjorngul@gmail.com>
2020-05-25 14:12:31 +0200

    Revert "testing heroku stuff"
    This reverts commit 7b5f7c562d1e2b3c9f4207b3f95e01b5ad1ff0c2.

--- server.js
+++ server.js
@@ -7,11 +7,30 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_CONNECTION_STRING;
+console.log(MONGODB_URI);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/api", router);
 app.use(express.static(path.join(__dirname, "client", "build")));
 
+app.get("/config.js", function (req, res) {
+  // this is probably a bad idea
+  res.write(
+    "var MONGODB_CONNECTION_STRING='" +
+      process.env.MONGODB_CONNECTION_STRING +
+      "'" +
+      "\n"
+  );
+  res.write("var AUTH0_DOMAIN='" + process.env.AUTH0_DOMAIN + "'" + "\n");
+  res.write("var AUTH0_CLIENT_ID='" + process.env.AUTH0_CLIENT_ID + "'" + "\n");
+  res.write(
+    "var AUTH0_CLIENT_SECRET='" + process.env.AUTH0_CLIENT_SECRET + "'" + "\n"
+  );
+  res.write(
+    "var AUTH0_CALLBACK_URL='" + process.env.AUTH0_CALLBACK_URL + "'" + "\n"
+  );
+  res.end();
+});
 mongoose.connect(
   MONGODB_URI,
   {
